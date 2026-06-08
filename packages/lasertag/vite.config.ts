70eb5c54ba6f6ca057493ba8154b03f97b174963
Jeremy Banka <hello@jeremybanka.com>
2026-06-08 11:26:43 -0700

    🐛 fix bundle; don't include eslint

--- packages/lasertag/vite.config.ts
+++ packages/lasertag/vite.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from "vite-plus"
 export default defineConfig({
 	pack: {
 		clean: true,
+		deps: {
+			dts: {
+				neverBundle: [/^[\w@]/],
+			},
+			onlyBundle: [],
+			skipNodeModulesBundle: true,
+		},
 		dts: true,
 		entry: ["eslint/src/plugin.ts"],
 		format: "esm",
